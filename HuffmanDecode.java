438a2022f7b7c10f0be4ac4178ded317ef29643a
Chris Mendoza <chrisjmendoza@gmail.com>
2015-11-22 21:28:30 -0800

    Create HuffmanDecode.java

---
+++ HuffmanDecode.java
@@ -0,0 +1,28 @@
+
+/**
+ * @author Chris Mendoza
+ *
+ */
+public class HuffmanDecode {
+
+	/**
+	 * Constructor
+	 * @param encodedFileName
+	 */
+	public HuffmanDecode(String encodedFileName) {
+
+	}
+
+	/**
+	 * This method accepts a compressed file and needs: Read the encoded
+	 * bit stream and decode the information for each matching bit sequence. Hint:
+	 * Since the encoded bit stream does not and cannot have separators, you need
+	 * to test each single sub-sequence against the existing codes!
+	 * 
+	 * @param decodedFileName
+	 */
+	public void readFromFile(String decodedFileName) {
+
+	}
+
+}
